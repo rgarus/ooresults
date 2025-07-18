a9fac2f23f7e07bf1de49dde493e256ef7d11b0d
Rainer Garus <118051020+rgarus@users.noreply.github.com>
2025-07-18 19:00:42 +0200

    webtests extended

---
+++ webtests/pageobjects/courses.py
@@ -0,0 +1,187 @@
+# Copyright (C) 2022 Rainer Garus
+#
+# This file is part of the ooresults Python package, a software to
+# compute results of orienteering events.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from typing import Optional
+from typing import TypeVar
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import invisibility_of_element
+from selenium.webdriver.support.wait import WebDriverWait
+
+from webtests.controls.text_control import TextControl
+from webtests.pageobjects.actions import Actions
+from webtests.pageobjects.table import Table
+
+
+T = TypeVar("T", bound="AddCourseDialog")
+
+
+class AddCourseDialog:
+    def __init__(self, page: webdriver.Remote) -> None:
+        self.page = page
+
+    def wait(self: T) -> T:
+        self.page.find_element(By.ID, "cou.formAdd")
+        return self
+
+    def check_values(
+        self,
+        name: str,
+        length: str,
+        climb: str,
+        controls: str,
+    ):
+        self.wait()
+
+        assert name == TextControl(page=self.page, id="cou_name").get_text()
+        assert length == TextControl(page=self.page, id="cou_length").get_text()
+        assert climb == TextControl(page=self.page, id="cou_climb").get_text()
+        assert controls == TextControl(page=self.page, id="cou_controls").get_text()
+
+    def enter_values(
+        self,
+        name: Optional[str] = None,
+        length: Optional[str] = None,
+        climb: Optional[str] = None,
+        controls: Optional[str] = None,
+    ):
+        self.wait()
+
+        if name is not None:
+            TextControl(page=self.page, id="cou_name").set_text(text=name)
+        if length is not None:
+            TextControl(page=self.page, id="cou_length").set_text(text=length)
+        if climb is not None:
+            TextControl(page=self.page, id="cou_climb").set_text(text=climb)
+        if controls is not None:
+            TextControl(page=self.page, id="cou_controls").set_text(text=controls)
+
+    def submit(self) -> None:
+        elem = self.page.find_element(By.ID, "cou.formAdd")
+        elem = elem.find_element(By.XPATH, "button[text()='Save']")
+        elem.click()
+        WebDriverWait(self.page, 10).until(invisibility_of_element(elem))
+
+    def cancel(self) -> None:
+        elem = self.page.find_element(By.ID, "cou.formAdd")
+        elem = elem.find_element(By.XPATH, "button[text()='Cancel']")
+        elem.click()
+        WebDriverWait(self.page, 10).until(invisibility_of_element(elem))
+
+
+class ImportCourseDialog:
+    def __init__(self, page: webdriver.Remote) -> None:
+        self.page = page
+
+    def cancel(self) -> None:
+        elem = self.page.find_element(By.ID, "cou.formImport")
+        elem.find_element(By.XPATH, "button[text()='Cancel']").click()
+        WebDriverWait(self.page, 10).until(invisibility_of_element(elem))
+
+
+class ExportCourseDialog:
+    def __init__(self, page: webdriver.Remote) -> None:
+        self.page = page
+
+    def cancel(self) -> None:
+        elem = self.page.find_element(By.ID, "cou.formExport")
+        elem.find_element(By.XPATH, "button[text()='Cancel']").click()
+        WebDriverWait(self.page, 10).until(invisibility_of_element(elem))
+
+
+class DeleteCourseDialog:
+    def __init__(self, page: webdriver.Remote) -> None:
+        self.page = page
+
+    def ok(self) -> None:
+        elem = self.page.find_element(By.ID, "cou.formDelete")
+        elem.find_element(By.XPATH, "button[text()='Delete']").click()
+        WebDriverWait(self.page, 10).until(invisibility_of_element(elem))
+
+    def cancel(self) -> None:
+        elem = self.page.find_element(By.ID, "cou.formDelete")
+        elem.find_element(By.XPATH, "button[text()='Cancel']").click()
+        WebDriverWait(self.page, 10).until(invisibility_of_element(elem))
+
+
+class CoursePage:
+    def __init__(self, page: webdriver.Remote) -> None:
+        self.page = page
+        self.actions = CourseActions(page=page)
+        self.table = CourseTable(page=page)
+
+    def filter(self) -> TextControl:
+        return TextControl(page=self.page, id="cour.filter")
+
+    def get_event_name(self) -> str:
+        return self.page.find_element(By.ID, "cou.event_name").text
+
+    def get_event_date(self) -> str:
+        return self.page.find_element(By.ID, "cou.event_date").text
+
+    def delete_courses(self):
+        for i in range(self.table.nr_of_rows() - 1):
+            self.table.select_row(2)
+            self.actions.delete().ok()
+
+
+class CourseActions(Actions):
+    def __init__(self, page: webdriver.Remote) -> None:
+        super().__init__(page=page, id="cour.actions")
+
+    def reload(self) -> None:
+        self.action(text="Reload").click()
+
+    def import_(self) -> ImportCourseDialog:
+        self.action(text="Import ...").click()
+        return ImportCourseDialog(page=self.page)
+
+    def export(self) -> ExportCourseDialog:
+        self.action(text="Export ...").click()
+        return ExportCourseDialog(page=self.page)
+
+    def add(self) -> AddCourseDialog:
+        self.action(text="Add course ...").click()
+        return AddCourseDialog(page=self.page)
+
+    def edit(self) -> AddCourseDialog:
+        self.action(text="Edit course ...").click()
+        return AddCourseDialog(page=self.page)
+
+    def delete(self) -> DeleteCourseDialog:
+        self.action(text="Delete course").click()
+        return DeleteCourseDialog(page=self.page)
+
+
+class CourseTable(Table):
+    def __init__(self, page: webdriver.Remote) -> None:
+        super().__init__(page=page, xpath="//table[@id='cou.table']")
+
+    def selected_row(self) -> Optional[int]:
+        rows = self.selected_rows()
+
+        if len(rows) >= 2:
+            raise RuntimeError(f"Multiple rows selected: {rows}")
+        else:
+            return rows[0] if rows else None
+
+    def double_click_row(self, i: int) -> AddCourseDialog:
+        super().double_click_row(i=i)
+        return AddCourseDialog(page=self.page)
---
+++ webtests/tests/courses/test_courses.py
@@ -0,0 +1,473 @@
+# Copyright (C) 2022 Rainer Garus
+#
+# This file is part of the ooresults Python package, a software to
+# compute results of orienteering events.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import pytest
+from selenium import webdriver
+
+from webtests.pageobjects.courses import CoursePage
+from webtests.pageobjects.events import EventPage
+from webtests.pageobjects.tabs import Tabs
+
+
+EVENT_NAME = "Test for Courses"
+EVENT_DATE = "2023-12-28"
+
+
+@pytest.fixture(scope="module")
+def select_event(page: webdriver.Remote) -> None:
+    Tabs(page=page).select(text="Events")
+    event_page = EventPage(page=page)
+    event_page.delete_events()
+    dialog = event_page.actions.add()
+    dialog.enter_values(
+        name=EVENT_NAME,
+        date=EVENT_DATE,
+    )
+    dialog.submit()
+    event_page.table.select_row(2)
+    yield
+    Tabs(page=page).select(text="Events")
+    event_page = EventPage(page=page)
+    event_page.delete_events()
+
+
+@pytest.fixture
+def course_page(page: webdriver.Remote, select_event: None) -> CoursePage:
+    Tabs(page=page).select(text="Courses")
+    return CoursePage(page=page)
+
+
+@pytest.fixture
+def delete_courses(course_page: CoursePage) -> None:
+    course_page.delete_courses()
+
+
+@pytest.fixture
+def course(course_page: CoursePage, delete_courses: None) -> None:
+    dialog = course_page.actions.add()
+    dialog.enter_values(
+        name="Bahn B",
+        length="4500",
+        climb="60",
+        controls="122-123-124-125-121",
+    )
+    dialog.submit()
+    # check number of rows
+    assert course_page.table.nr_of_rows() == 2
+    assert course_page.table.nr_of_columns() == 4
+
+
+def test_if_course_page_is_displayed_then_all_actions_are_displayed(
+    course_page: CoursePage,
+):
+    assert course_page.actions.texts() == [
+        "Reload",
+        "Import ...",
+        "Export ...",
+        "Add course ...",
+        "Edit course ...",
+        "Delete course",
+    ]
+
+
+def test_if_no_row_is_selected_then_some_actions_are_disabled(
+    course_page: CoursePage, course: None
+):
+    assert course_page.actions.action("Reload").is_enabled()
+    assert course_page.actions.action("Import ...").is_enabled()
+    assert course_page.actions.action("Export ...").is_enabled()
+    assert course_page.actions.action("Add course ...").is_enabled()
+    assert course_page.actions.action("Edit course ...").is_disabled()
+    assert course_page.actions.action("Delete course").is_disabled()
+
+
+def test_if_a_row_is_selected_then_all_actions_are_enabled(
+    course_page: CoursePage, course: None
+):
+    course_page.table.select_row(i=2)
+
+    assert course_page.actions.action("Reload").is_enabled()
+    assert course_page.actions.action("Import ...").is_enabled()
+    assert course_page.actions.action("Export ...").is_enabled()
+    assert course_page.actions.action("Add course ...").is_enabled()
+    assert course_page.actions.action("Edit course ...").is_enabled()
+    assert course_page.actions.action("Delete course").is_enabled()
+
+
+def test_if_course_page_is_selected_then_the_table_header_is_displayed(
+    course_page: CoursePage,
+):
+    assert course_page.table.nr_of_columns() == 4
+    assert course_page.table.headers() == [
+        "Name",
+        "Length",
+        "Climb",
+        "Controls",
+    ]
+
+
+def test_if_a_course_is_added_with_required_data_then_an_additional_course_is_displayed(
+    course_page: CoursePage, delete_courses: None
+):
+    dialog = course_page.actions.add()
+    dialog.check_values(
+        name="",
+        length="",
+        climb="",
+        controls="",
+    )
+    dialog.enter_values(
+        name="Bahn A",
+        length="",
+        climb="",
+        controls="",
+    )
+    dialog.submit()
+
+    # check number of rows
+    assert course_page.table.nr_of_rows() == 2
+    assert course_page.table.nr_of_columns() == 4
+
+    assert course_page.table.row(i=1) == [
+        "Courses  (1)",
+    ]
+    assert course_page.table.row(i=2) == [
+        "Bahn A",
+        "",
+        "",
+        "",
+    ]
+
+
+def test_if_adding_a_course_is_cancelled_then_no_additional_course_is_displayed(
+    course_page: CoursePage, delete_courses: None
+):
+    dialog = course_page.actions.add()
+    dialog.enter_values(
+        name="Bahn A",
+        length="",
+        climb="",
+        controls="121-124-122-123",
+    )
+    dialog.cancel()
+
+    # check number of rows
+    assert course_page.table.nr_of_rows() == 0
+    assert course_page.table.nr_of_columns() == 4
+
+
+def test_if_a_course_is_added_with_all_data_then_an_additional_course_is_displayed(
+    course_page: CoursePage, delete_courses: None
+):
+    dialog = course_page.actions.add()
+    dialog.check_values(
+        name="",
+        length="",
+        climb="",
+        controls="",
+    )
+    dialog.enter_values(
+        name="Bahn B",
+        length="3600",
+        climb="75",
+        controls="121-124-122-123",
+    )
+    dialog.submit()
+
+    # check number of rows
+    assert course_page.table.nr_of_rows() == 2
+    assert course_page.table.nr_of_columns() == 4
+
+    assert course_page.table.row(i=1) == [
+        "Courses  (1)",
+    ]
+    assert course_page.table.row(i=2) == [
+        "Bahn B",
+        "3600",
+        "75",
+        "121 - 124 - 122 - 123",
+    ]
+
+
+def test_if_no_course_is_selected_and_a_new_course_is_added_then_no_course_is_selected(
+    course_page: CoursePage, course: None
+):
+    dialog = course_page.actions.add()
+    dialog.enter_values(
+        name="Bahn A",
+        length="",
+        climb="",
+        controls="121-124-122-123",
+    )
+    dialog.submit()
+    assert course_page.table.selected_row() is None
+
+
+def test_if_a_new_course_is_added_then_no_course_is_selected(
+    course_page: CoursePage, course: None
+):
+    course_page.table.select_row(i=2)
+    assert course_page.table.selected_row() == 2
+
+    dialog = course_page.actions.add()
+    dialog.enter_values(
+        name="Bahn A",
+        length="",
+        climb="",
+        controls="121-124-122-123",
+    )
+    dialog.submit()
+    assert course_page.table.selected_row() is None
+
+
+def test_if_a_course_is_edited_then_the_changed_data_are_displayed(
+    course_page: CoursePage, course: None
+):
+    course_page.table.select_row(2)
+
+    dialog = course_page.actions.edit()
+    dialog.check_values(
+        name="Bahn B",
+        length="4500",
+        climb="60",
+        controls="122 - 123 - 124 - 125 - 121",
+    )
+    dialog.enter_values(
+        name="Bahn easy",
+        length="4650",
+        climb="45",
+        controls="122 - 124 - 125",
+    )
+    dialog.submit()
+
+    # check number of rows
+    assert course_page.table.nr_of_rows() == 2
+    assert course_page.table.nr_of_columns() == 4
+
+    assert course_page.table.row(i=1) == [
+        "Courses  (1)",
+    ]
+    assert course_page.table.row(i=2) == [
+        "Bahn easy",
+        "4650",
+        "45",
+        "122 - 124 - 125",
+    ]
+
+
+def test_if_a_row_is_double_clicked_the_edit_dialog_is_opened(
+    course_page: CoursePage, course: None
+):
+    dialog = course_page.table.double_click_row(2)
+    dialog.check_values(
+        name="Bahn B",
+        length="4500",
+        climb="60",
+        controls="122 - 123 - 124 - 125 - 121",
+    )
+    dialog.enter_values(
+        name="Bahn easy",
+        length="4650",
+        climb="45",
+        controls="122 - 124 - 125",
+    )
+    dialog.submit()
+
+    # check number of rows
+    assert course_page.table.nr_of_rows() == 2
+    assert course_page.table.nr_of_columns() == 4
+
+    assert course_page.table.row(i=1) == [
+        "Courses  (1)",
+    ]
+    assert course_page.table.row(i=2) == [
+        "Bahn easy",
+        "4650",
+        "45",
+        "122 - 124 - 125",
+    ]
+
+
+def test_if_a_course_is_deleted_then_the_course_is_no_longer_displayed(
+    course_page: CoursePage, course: None
+):
+    # add a second course
+    dialog = course_page.actions.add()
+    dialog.enter_values(
+        name="Bahn A",
+        length="",
+        climb="",
+        controls="121-124-122-123",
+    )
+    dialog.submit()
+
+    # select course
+    course_page.table.select_row(3)
+
+    # delete course
+    course_page.actions.delete().ok()
+
+    # check number of rows
+    assert course_page.table.nr_of_rows() == 2
+    assert course_page.table.nr_of_columns() == 4
+
+    assert course_page.table.row(i=1) == [
+        "Courses  (1)",
+    ]
+    assert course_page.table.row(i=2) == [
+        "Bahn A",
+        "",
+        "",
+        "121 - 124 - 122 - 123",
+    ]
+
+
+def test_if_a_course_is_deleted_then_no_courser_is_selected(
+    course_page: CoursePage, course: None
+):
+    # add a second course
+    dialog = course_page.actions.add()
+    dialog.enter_values(
+        name="Bahn A",
+        length="",
+        climb="",
+        controls="121-124-122-123",
+    )
+    dialog.submit()
+
+    # select course
+    course_page.table.select_row(2)
+
+    # delete course
+    course_page.actions.delete().ok()
+    assert course_page.table.selected_row() is None
+
+
+def test_if_deleting_a_course_is_cancelled_then_the_course_is_displayed_further(
+    course_page: CoursePage, course: None
+):
+    # select course
+    course_page.table.select_row(2)
+    assert course_page.table.selected_row() == 2
+
+    # cancel deleting the course
+    course_page.actions.delete().cancel()
+
+    # check number of rows
+    assert course_page.table.nr_of_rows() == 2
+    assert course_page.table.row(i=1) == ["Courses  (1)"]
+    assert course_page.table.row(i=2) == [
+        "Bahn B",
+        "4500",
+        "60",
+        "122 - 123 - 124 - 125 - 121",
+    ]
+
+
+def test_if_several_courses_are_added_then_the_added_courses_are_displayed(
+    course_page: CoursePage, course: None
+):
+    dialog = course_page.actions.add()
+    dialog.enter_values(
+        name="Bahn C",
+        length="",
+        climb="",
+        controls="130-131-132",
+    )
+    dialog.submit()
+
+    dialog = course_page.actions.add()
+    dialog.enter_values(
+        name="Bahn A",
+        length="",
+        climb="",
+        controls="121-124-122-123",
+    )
+    dialog.submit()
+
+    # check number of rows
+    assert course_page.table.nr_of_rows() == 4
+    assert course_page.table.nr_of_columns() == 4
+
+    assert course_page.table.row(i=1) == [
+        "Courses  (3)",
+    ]
+    assert course_page.table.row(i=2) == [
+        "Bahn A",
+        "",
+        "",
+        "121 - 124 - 122 - 123",
+    ]
+    assert course_page.table.row(i=3) == [
+        "Bahn B",
+        "4500",
+        "60",
+        "122 - 123 - 124 - 125 - 121",
+    ]
+    assert course_page.table.row(i=4) == [
+        "Bahn C",
+        "",
+        "",
+        "130 - 131 - 132",
+    ]
+
+
+def test_if_filter_is_set_then_only_matching_rows_are_displayed(
+    course_page: CoursePage, course: None
+):
+    dialog = course_page.actions.add()
+    dialog.enter_values(
+        name="Bahn C",
+        length="",
+        climb="",
+        controls="130-131-132",
+    )
+    dialog.submit()
+
+    dialog = course_page.actions.add()
+    dialog.enter_values(
+        name="Bahn A",
+        length="",
+        climb="",
+        controls="121-124-122-123",
+    )
+    dialog.submit()
+
+    # check number of rows
+    assert course_page.table.nr_of_rows() == 4
+    assert course_page.table.nr_of_columns() == 4
+
+    try:
+        course_page.filter().set_text("Bahn B")
+
+        # check number of rows
+        assert course_page.table.nr_of_rows() == 2
+        assert course_page.table.nr_of_columns() == 4
+
+        assert course_page.table.row(i=1) == [
+            "Courses  (3)",
+        ]
+        assert course_page.table.row(i=2) == [
+            "Bahn B",
+            "4500",
+            "60",
+            "122 - 123 - 124 - 125 - 121",
+        ]
+    finally:
+        course_page.filter().set_text("")
