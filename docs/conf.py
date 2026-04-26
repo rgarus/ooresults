4130e9af325e50267666ef4b10a42f92647fa3f3
Rainer Garus <118051020+rgarus@users.noreply.github.com>
2026-04-26 12:23:09 +0200

    CHANGELOG updated for version 0.4.8

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ from multiproject.utils import get_project
 
 copyright = "2022, rgarus"
 author = "rgarus"
-release = "0.4.7"
+release = "0.4.8"
 
 
 # -- General configuration ---------------------------------------------------
